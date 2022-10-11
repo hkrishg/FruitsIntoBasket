e9d0a234aa062b8a8e193758e3ce9e325dfc008e
hkrishg <109535284+hkrishg@users.noreply.github.com>
2022-10-11 01:20:47 +0000

    formatted

--- Main.java
+++ Main.java
@@ -2,26 +2,30 @@ import java.util.*;
 
 class Main {
 
+  // Given an array of characters where each character represents a fruit tree,
+  // you are given two baskets and your goal is to put maximum number of fruits in
+  // each basket. The only restriction is that each basket can have only one type
+  // of fruit.
 
-//   Given an array of characters where each character represents a fruit tree, you are given two baskets and your goal is to put maximum number of fruits in each basket. The only restriction is that each basket can have only one type of fruit.
+  // You can start with any tree, but once you have started you can’t skip a tree.
+  // You will pick one fruit from each tree until you cannot, i.e., you will stop
+  // when you have to pick from a third fruit type.
 
-// You can start with any tree, but once you have started you can’t skip a tree. You will pick one fruit from each tree until you cannot, i.e., you will stop when you have to pick from a third fruit type.
+  // Write a function to return the maximum number of fruits in both the baskets.
 
-// Write a function to return the maximum number of fruits in both the baskets.
+  // Example 1:
 
-// Example 1:
+  // Input: Fruit=['A', 'B', 'C', 'A', 'C']
+  // Output: 3
+  // Explanation: We can put 2 'C' in one basket and one 'A' in the other from the
+  // subarray ['C', 'A', 'C']
+  // Example 2:
 
-// Input: Fruit=['A', 'B', 'C', 'A', 'C']
-// Output: 3
-// Explanation: We can put 2 'C' in one basket and one 'A' in the other from the subarray ['C', 'A', 'C']
-// Example 2:
-
-// Input: Fruit=['A', 'B', 'C', 'B', 'B', 'C']
-// Output: 5
-// Explanation: We can put 3 'B' in one basket and two 'C' in the other basket. 
-// This can be done if we start with the second letter: ['B', 'C', 'B', 'B', 'C']
-
-
+  // Input: Fruit=['A', 'B', 'C', 'B', 'B', 'C']
+  // Output: 5
+  // Explanation: We can put 3 'B' in one basket and two 'C' in the other basket.
+  // This can be done if we start with the second letter: ['B', 'C', 'B', 'B',
+  // 'C']
 
   private static int maxFruitCountOf2Types(char[] input) {
     int windowStart = 0, maxLength = 0;
@@ -42,7 +46,7 @@ class Main {
     return maxLength;
   }
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
     int result = Main.maxFruitCountOf2Types(new char[] { 'A', 'B', 'C', 'A', 'C' });
     System.out.println("----" + result);
   }
